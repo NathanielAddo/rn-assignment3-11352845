b92c6a10f604e8e3029ca9ffe74eef38aa4384c3
NathanielAddo <stevenelly100@gmail.com>
2024-06-01 18:41:43 +0000

    Add welcome/greeting component

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView,  TextInput, Button, FlatList, SectionList } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+      <View style={styles.container}>
+    <View style={styles.headDiv} >
+      <Text style={styles.headText}>Hello , Devs</Text>
+      <Image style={styles.profileImage} source={require('./profile.png')}></Image>
+      </View>
+
+      <View>
+      <Text>14 tasks today</Text>
+      </View>
+
+      <View style={styles.categories}>
+      <Text style={styles.headText}>Categories</Text>
+      </View>
+      
+    
+    <ScrollView> </ScrollView>
+    <StatusBar style="auto" />
     </View>
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: '#F7F0E8',
+    alignItems: '',
+    justifyContent: '',
+  },
+  headDiv: {
+    flexDirection: 'row',
+  },
+  headText: {
+    flex: 4,
+    fontSize: 18,
+    fontWeight: 'Bold',
+    color: '#000000',
+    alignItems: 'left',
+    justifyContent: '',
+  },
+  categories: {
+    marginTop: 50,
+    alignItems: '',
+    justifyContent: '',
+  },
+  profileImage: {
+    width: 24,
+    alignItems: 'right',
+    height: 24,
+  },
+  searchBar: {
+    paddingLeft: 8,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 5,
   },
 });
