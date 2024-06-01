ef1655346e00ff7e1151fdb0484ecc952e9eaea0
NathanielAddo <stevenelly100@gmail.com>
2024-06-01 23:02:00 +0000

    Add categories section

--- App.js
+++ App.js
@@ -3,6 +3,42 @@ import { StyleSheet, Text, View, Image, ScrollView,  TextInput, Button, FlatList
 
 
 export default function App() {
+  const data = [
+    {
+      id: 1,
+      title: "Exercise",
+      subtitle: "12 Tasks",
+      image: require('./girl-sitting-on-floor.png'),
+    },
+    {
+      id: 2,
+      title: "Study",
+      subtitle: "12 Tasks",
+      image: require('./girl-in-workspace.png'),
+    },
+  ];
+  const tasks = [
+    {
+      id:1,
+      title: "Mobile App Development"
+    },
+    {
+      id: 2,
+      title: "Web Development"
+    },
+    {
+      id: 3,
+      title: "Push Ups"
+    },
+    {
+      id: 4,
+      title: "Mow The Lawn"
+    },
+    {
+      id: 5,
+      title: "Walk The Dogs"
+    },
+  ];
   return (
     
       <View style={styles.container}>
@@ -12,18 +48,34 @@ export default function App() {
       </View>
 
       <View>
-      <Text>14 tasks today</Text>
+      <Text style={styles.subtitle}>14 tasks today</Text>
       </View>
       <View>
         <TextInput placeholder="Search" style={styles.searchBar}/>
       </View>
-
+       
+      <ScrollView>
       <View style={styles.categories}>
-      <Text style={styles.headText}>Categories</Text>
+      <Text style={styles.categoriesText}>Categories</Text>
       </View>
       
+      <FlatList  horizontal={true} height={225}
+      showsVerticalScrollIndicator={false}
+      keyExtractor={(item) => item.id}
+      data={data} 
+      renderItem={({item}) => (
+        <View style={styles.categoriesContainer}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.subtitle}>{item.subtitle}</Text>
+          <Image source={item.image} style={styles.image}/>
+        </View>
+  )}
     
-    <ScrollView> </ScrollView>
+      />
+      <View>
+        <Text style={styles.categoriesText}>Ongoing Tasks</Text>
+      </View>
+     </ScrollView>
     <StatusBar style="auto" />
     </View>
    
@@ -45,25 +97,56 @@ const styles = StyleSheet.create({
   },
   headText: {
     flex: 4,
-    fontSize: 18,
-    fontWeight: 'Bold',
+    fontSize: 32,
+    fontWeight: 700,
     color: '#000000',
     alignItems: 'left',
     justifyContent: '',
+    lineHeight: 38.4,
+  },
+  profileImage: {
+    width: 46,
+    alignItems: 'right',
+    height: 45,
+  },
+  searchBar: {
+    paddingLeft: 8,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 5,
   },
   categories: {
     marginTop: 50,
     alignItems: '',
     justifyContent: '',
   },
-  profileImage: {
-    width: 24,
-    alignItems: 'right',
-    height: 24,
+  categoriesText: {
+    fontSize: 20,
+    fontWeight: 700,
+    color: '#000000',
+    alignItems: 'left',
+    justifyContent: '',
   },
-  searchBar: {
-    paddingLeft: 8,
+  categoriesContainer: {
+    height: 225,
     backgroundColor: '#FFFFFF',
-    borderRadius: 5,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 700,
+    marginLeft: 16,
+    alignItems: '',
+    justifyContent: '',
+  },
+  subtitle: {
+    fontSize: 12,
+    fontWeight: 500,
+    marginLeft: 16,
+    alignItems: '',
+    justifyContent: '',
+  },
+  image: {
+    width: 396,
+    alignItems: 'right',
+    height: 192,
   },
 });
